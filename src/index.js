8f291f6ad4b80669875c042f21da279859f2fbdd
GiovanniCarboni <giovanni.carboni.1997@gmail.com>
2022-11-10 16:44:10 +0200

    sounds added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+const keyboard = document.querySelector("#keyboard");
+const do1 = document.getElementById("do");
 
+console.log(do1);
+
+keyboard.addEventListener("mousedown", function (e) {
+  const key = e.target;
+  key.style.boxShadow = "inset 2px 0px 5px gray, inset -2px -2px 5px gray";
+  key.querySelector(".sound").cloneNode(true).play();
+});
+
+["mouseup", "mouseout"].forEach((event) => {
+  keyboard.addEventListener(event, function (e) {
+    e.target.style.boxShadow = "";
+  });
+});
