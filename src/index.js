6b045e5ce05da8ea6740259bb5c5215c407a329e
GiovanniCarboni <giovanni.carboni.1997@gmail.com>
2022-11-10 19:36:13 +0200

    added pedal

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 const keyboard = document.querySelector("#keyboard");
+const rightPedal = document.querySelector("#right-pedal");
 
+// pedal
+let pedalPressed;
+rightPedal.addEventListener("click", function (e) {
+  if (pedalPressed) {
+    rightPedal.style.transform = "";
+    pedalPressed = false;
+    return;
+  }
+  rightPedal.style.transform = "translateY(50%)";
+  pedalPressed = true;
+});
+
+// keys
 let pressedKey;
-
 keyboard.addEventListener("mousedown", function (e) {
   const key = e.target;
-  key.style.boxShadow = "inset 2px 0px 5px gray, inset -2px -2px 5px gray";
-  if (pressedKey) pressedKey.pause();
+  key.style.boxShadow = "inset 2px 0px 5px gray, inset -2px -3px 5px gray";
+  if (!pedalPressed && pressedKey) pressedKey.pause();
   pressedKey = key.querySelector(".sound").cloneNode(true);
   pressedKey.play();
 });
