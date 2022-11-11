76f82ceb90111a6b90f56732928a40b3f8bd87e4
GiovanniCarboni <giovanni.carboni.1997@gmail.com>
2022-11-11 16:35:22 +0200

    changed functionality

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import * as note from "./sounds.js";
+
 const keyboard = document.querySelector("#keyboard");
 const rightPedal = document.querySelector("#right-pedal");
 
-// pedal
 let pedalPressed;
 rightPedal.addEventListener("click", function (e) {
   if (pedalPressed) {
+    Howler.stop();
     rightPedal.style.transform = "";
     pedalPressed = false;
     return;
@@ -13,18 +15,17 @@ rightPedal.addEventListener("click", function (e) {
   pedalPressed = true;
 });
 
-// keys
-let pressedKey;
 keyboard.addEventListener("mousedown", function (e) {
   const key = e.target;
-  key.style.boxShadow = "inset 2px 0px 5px gray, inset -2px -3px 5px gray";
-  if (!pedalPressed && pressedKey) pressedKey.pause();
-  pressedKey = key.querySelector(".sound").cloneNode(true);
-  pressedKey.play();
+  if (!pedalPressed) {
+    Howler.stop(), 100;
+  }
+  key.style.boxShadow = "none";
+  note[key.id].play();
 });
 
 ["mouseup", "mouseout"].forEach((event) => {
   keyboard.addEventListener(event, function (e) {
-    e.target.style.boxShadow = "";
+    e.target.style.boxShadow = "inset 0px -5px rgb(112, 112, 109)";
   });
 });
---
+++ src/sounds.js
@@ -0,0 +1,74 @@
+import { Howl, Howler } from "howler";
+
+export const c4 = new Howl({
+  src: require(`./sounds/c4.ogg`),
+});
+export const cD4 = new Howl({
+  src: require(`./sounds/cD4.ogg`),
+});
+export const d4 = new Howl({
+  src: require(`./sounds/d4.ogg`),
+});
+export const dD4 = new Howl({
+  src: require(`./sounds/dD4.ogg`),
+});
+export const e4 = new Howl({
+  src: require(`./sounds/e4.ogg`),
+});
+export const f4 = new Howl({
+  src: require(`./sounds/f4.ogg`),
+});
+export const fD4 = new Howl({
+  src: require(`./sounds/fD4.ogg`),
+});
+export const g4 = new Howl({
+  src: require(`./sounds/g4.ogg`),
+});
+export const gD4 = new Howl({
+  src: require(`./sounds/gD4.ogg`),
+});
+export const a4 = new Howl({
+  src: require(`./sounds/a4.ogg`),
+});
+export const aD4 = new Howl({
+  src: require(`./sounds/aD4.ogg`),
+});
+export const b4 = new Howl({
+  src: require(`./sounds/b4.ogg`),
+});
+export const c5 = new Howl({
+  src: require(`./sounds/c5.ogg`),
+});
+export const cD5 = new Howl({
+  src: require(`./sounds/cD5.ogg`),
+});
+export const d5 = new Howl({
+  src: require(`./sounds/d5.ogg`),
+});
+export const dD5 = new Howl({
+  src: require(`./sounds/dD5.ogg`),
+});
+export const e5 = new Howl({
+  src: require(`./sounds/e5.ogg`),
+});
+export const f5 = new Howl({
+  src: require(`./sounds/f5.ogg`),
+});
+export const fD5 = new Howl({
+  src: require(`./sounds/fD5.ogg`),
+});
+export const g5 = new Howl({
+  src: require(`./sounds/g5.ogg`),
+});
+export const gD5 = new Howl({
+  src: require(`./sounds/gD5.ogg`),
+});
+export const a5 = new Howl({
+  src: require(`./sounds/a5.ogg`),
+});
+export const aD5 = new Howl({
+  src: require(`./sounds/aD5.ogg`),
+});
+export const b5 = new Howl({
+  src: require(`./sounds/b5.ogg`),
+});
